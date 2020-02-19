79db7992f7f854787d3ae7dea88074d39def2fe2
Evgeniya Ryabokon <ryabokon_evgeniya@apri-code.ru>
2020-02-19 17:25:26 +0500

    contact form

--- src/components/reauest-form/request-form.component.jsx
+++ src/components/reauest-form/request-form.component.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './request-form.style.sass';
 import house from "../../assets/images/house.png"
 
 export function Request(props) {
+        const [name, setName] = useState("");
+        const [number, setNumber] = useState("");
+        const [email, setEmail] = useState("");
+        const [isImageView, setIsImageView] = useState(false);
+    
+    function handleClickGetSmeta() {
+        console.log(name, number, email)
+    }
+
+    function handleClickViewImage(e) {
+        
+    }
+
     return (
       <div className="request-form">
           <div className="request-form__title">
@@ -12,9 +25,10 @@ export function Request(props) {
           </div>
           <div className="request-form__body">
             <div className="request-form__body_house">
-                <img className="request-form__body_house_image" src={house} alt="house" />
+                <img className="request-form__body_house_image" onClick={handleClickViewImage} src={house} alt="house" />
                 <figcaption>68 м2, Москва</figcaption> 
                 <figcaption>Двухкомнатная квартира</figcaption> 
+                {isImageView &&(<div className="request-form__body_house_imageView"></div>)}
             </div>
             <div className="request-form__body_card">
                 <div className="request-form__body_card_title">
@@ -22,21 +36,22 @@ export function Request(props) {
                 </div>
                 <div className="request-form__body_card_name">
                     <p>Введите ваше имя</p>
-                    <input type="text" />
+                    <input value={name} type="text" onChange={e => setName(e.target.value)} />
                 </div>
                 <div className="request-form__body_card_number">
                     <p>Введите ваш телефон</p>
-                    <input  type="tel" />
+                    <input value={number} type="tel" onChange={e => setNumber(e.target.value)} />
                 </div>
                 <div className="request-form__body_card_mail">
                     <p>Введите вашу почту</p>
-                    <input  type="email" />
+                    <input value={email} type="email" onChange={e => setEmail(e.target.value)} />
                 </div>
                 <div className="request-form__body_card_button">
-                    <button>Получить смету</button>
+                    <button onClick={handleClickGetSmeta}>Получить смету</button>
                 </div>
             </div>
           </div>
       </div>
     );
 }
+
